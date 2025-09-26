896b235e55b0c1658913df5a28ca8a59d3192bd9
k-waa <khirota309@gmail.com>
2025-09-27 00:21:55 +0900

    修正

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -4,46 +4,42 @@ import Stripe from "stripe"
 
 // 購入履歴の保存
 export async function POST(request: Request) {
-    if(process.env.STRIPE_SECRET_KEY) {
-        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-        const { sessionId } = await request.json();
-    
-        try {
-            const session = await stripe.checkout.sessions.retrieve(sessionId);
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+    const { sessionId } = await request.json();
 
-            if(session.metadata && session.client_reference_id) {
-                const existingPurchase = prisma.purchase.findFirst({
-                    where: {
-                        userId: session.client_reference_id,
-                        bookId: session.metadata.bookId
-                    }
-                })
+    try {
+        const session = await stripe.checkout.sessions.retrieve(sessionId);
 
-                // バグ要修正
-                // if(!existingPurchase) {
-                const purchase = await prisma.purchase.create({
-                    data: {
-                        userId: session.client_reference_id,
-                        bookId: session.metadata.bookId,
-                    }
-                });
+        if(session.metadata && session.client_reference_id) {
+            const existingPurchase = prisma.purchase.findFirst({
+                where: {
+                    userId: session.client_reference_id,
+                    bookId: session.metadata.bookId
+                }
+            })
 
-                return NextResponse.json({ purchase, existingPurchase });
-                // } else {
-                //     return NextResponse.json({message: "すでに購入済みです"})
-                // }
-            } else {
-                return NextResponse.json({ message: 'invalid params' })
-            }
-            
-        } catch (err) {
-            if(err instanceof Error) {
-                return NextResponse.json({ message: err.message })
-            }
-            
-            return NextResponse.json({ message: 'an unknown error' })
+            // バグ要修正
+            // if(!existingPurchase) {
+            const purchase = await prisma.purchase.create({
+                data: {
+                    userId: session.client_reference_id,
+                    bookId: session.metadata.bookId,
+                }
+            });
+
+            return NextResponse.json({ purchase, existingPurchase });
+            // } else {
+            //     return NextResponse.json({message: "すでに購入済みです"})
+            // }
+        } else {
+            return NextResponse.json({ message: 'invalid params' })
         }
-    } else {
-        return NextResponse.json({ message: 'apiKey not found.' })
+        
+    } catch (err) {
+        if(err instanceof Error) {
+            return NextResponse.json({ message: err.message })
+        }
+        
+        return NextResponse.json({ message: 'an unknown error' })
     }
 }
